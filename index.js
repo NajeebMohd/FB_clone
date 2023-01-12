ea85fd41e74791ac2ee44cb9c122c94c724a7cf8
najeeb <mohdnajeeb9642@gmail.com>
2023-01-12 09:26:02 +0530

    small fix in session mongo

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(session({
     },
     store:MongoStore.create(
         {
-            mongoUrl : 'mongodb://127.0.0.1:27017/NodejsAuthentication',
+            mongoUrl : process.env.mongodburl,
             autoRemove : 'Disabled',
             mongoOptions : {}
         },
