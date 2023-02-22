0395eaa5290faf88507ace21fa3997b1c4377ccb
najeeb <mohdnajeeb9642@gmail.com>
2023-02-22 17:07:43 +0530

    done deleting post

--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -1,12 +1,36 @@
 const Post = require('../models/Post.js');
 const User = require('../models/user');
+const Comment = require('../models/comment');
 
 module.exports.CreatePost = function(req,res){
-    Post.create({
-        user: req.user.id,
-        caption : req.body.caption
-    },function(err){
-        if(err){console.log('error while creating a post -> ',err);return;}
+    
+    User.findById(req.user.id,function(err,user){
+        if(err){console.log('err in the create-post --> ',err);return}
+        if(user){
+            Post.create({
+                user: req.user.id,
+                caption : req.body.caption
+            },function(err){
+                if(err){console.log('error while creating a post -> ',err);return}                
+            })
+        }
         return res.redirect('back');
     })
+}
+module.exports.DeletePost = async function(req,res){
+    try{
+        let post = await Post.findById(req.params.id);
+
+        if(post.user == req.user.id){
+            post.remove();
+            await Comment.deleteMany({post: req.params.id});
+
+            return res.redirect('back');
+        }else{
+            return res.redirect('back');
+        }
+    }catch(err){
+        console.log('error in the delete-post --> ',err);
+        return res.redirect('back');
+    }
 }
--- routes/post.js
+++ routes/post.js
@@ -5,5 +5,6 @@ const passport = require('passport');
 const PostController = require('../controller/posts_controller');
 
 router.post('/create-post',passport.checkAuthentication,PostController.CreatePost);
+router.get('/delete-post/:id', passport.checkAuthentication, PostController.DeletePost);
 
 module.exports = router;
