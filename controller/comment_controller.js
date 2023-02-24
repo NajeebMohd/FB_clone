4ff93447f0609297eef1689a6b8e607f90080944
najeeb <mohdnajeeb9642@gmail.com>
2023-02-24 12:03:42 +0530

    done updating the funtions

--- controller/comment_controller.js
+++ controller/comment_controller.js
@@ -1,37 +1,35 @@
 const Comment = require('../models/comment');
 const Post = require('../models/Post');
-module.exports.CreateComment = function(req,res){
-    
-    Comment.create({
-        user:req.user.id,
-        post : req.body.postId,
-        content : req.body.content
-    },function(err,comment){
-        if(err){
-            console.log(err,' in the create comment ');
-            return;
-        }
-        Post.findById(req.body.postId,function(err,post){
-            if(err){console.log('err in cc',err);return}            
+
+module.exports.CreateComment = async function(req,res){
+    try{
+        let post = await Post.findById(req.body.postId);
+        if(post){
+            let comment = await Comment.create({
+                user : req.user.id,
+                post : req.body.postId,
+                content : req.body.content
+            });
             post.comments.push(comment._id);
             post.save();
-        })
-        
-    });
-    return res.redirect('back');
+        }
+        return res.redirect('back');
+    }catch(err){
+        console.log('error while creating the comment --> ',err);
+        return res.redirect('back');
+    }
 }
 
-module.exports.DeleteComment = function(req,res){
-    Comment.findById(req.params.id,function(err,comment){
-        if(err){console.log('error in the delete-comment --> ',err);return}
-        if(comment.user == req.user.id){
-            let postId = comment.post;
-            Post.findByIdAndUpdate(postId,{$pull : {comments : req.params.id}}, function(err,post){
-                if(err){console.log('error in the delete-comment --> ',err);return}
-                return res.redirect('back');
-            })
-        }else{
-            return res.redirect('back');
+module.exports.DeleteComment = async function(req,res){
+    try{
+        let comment = await Comment.findById(req.params.id);
+        if(comment.user == req.user.id){            
+            await Post.findByIdAndUpdate(comment.post,{$pull : {comments : req.params.id}});
+            comment.remove();
         }
-    })   
+        return res.redirect('back');
+    }catch(err){
+        console.log('error while deleting the comment --> ',err);
+        return res.redirect('back');
+    }    
 }
